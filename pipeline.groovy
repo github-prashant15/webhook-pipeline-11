37272fa61cdba1420c67448a4964178e323ba5d4
prashant <prashantsp1507@gmail.com>
2022-04-05 14:47:30 +0530

    first commit

---
+++ pipeline.groovy
@@ -0,0 +1,20 @@
+pipeline {
+        agent any 
+        stages {
+          stage('Build') { 
+            steps {
+              echho "Build is completed"
+            }
+          }
+          stage('Test') { 
+            steps {
+              echo "Test is completed"
+            }
+          }
+          stage('Deploy') { 
+            steps {
+              echo "Deploy is completed"
+            }
+          }
+        }
+      }
